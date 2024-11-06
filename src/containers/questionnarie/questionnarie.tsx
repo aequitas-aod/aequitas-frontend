8980ea00318db13352ce34786a42e9d2bc20e45d
Jessica Corrias <corrias.jessica93@gmail.com>
2024-11-06 14:34:25 +0100

    fix: missing Suspense boundary with useSearchParams

---
+++ src/containers/questionnarie/questionnarie.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { DataMitigationPage } from "@/features/data-mitigation/page";
+import { DatasetChoicePage } from "@/features/dataset-choice/page";
+import { DatasetViewPage } from "@/features/dataset-view/page";
+import { DependenciesPage } from "@/features/dependencies/page";
+import { DetectionPage } from "@/features/detection/page";
+import { FeatureViewPage } from "@/features/feature-view/page";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+export default function QuestionnarieContainer() {
+  const searchParams = useSearchParams();
+
+  const [question, setQuestion] = useState(searchParams.get("question") || "1");
+
+  const incrementQuestion = () => {
+    const nextQuest = (parseInt(question, 10) + 1).toString();
+    setQuestion(nextQuest);
+  };
+
+  return (
+    <div>
+      {question === "1" && <DatasetChoicePage onNext={incrementQuestion} />}
+      {question === "2" && <DatasetViewPage />}
+      {question === "3" && <FeatureViewPage />}
+      {question === "4" && <DependenciesPage />}
+      {question === "5" && <DetectionPage />}
+      {question === "6" && <DataMitigationPage />}
+    </div>
+  );
+}
