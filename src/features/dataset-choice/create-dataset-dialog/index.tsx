911abe48db2730e672e579c77c47b92dd5b08830
Jessica Corrias <corrias.jessica93@gmail.com>
2024-11-12 11:15:16 +0100

    feat: enhance CreateDatasetDialog with file upload functionality and validation improvements

--- src/features/dataset-choice/create-dataset-dialog/index.tsx
+++ src/features/dataset-choice/create-dataset-dialog/index.tsx
@@ -2,7 +2,6 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -21,14 +20,22 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useTranslations } from "next-intl";
 import { Textarea } from "@/components/ui/textarea";
 import { FormSchema, FormValues } from "./schema";
+import { Label } from "@/components/ui/label";
+import { CheckIcon, PlusIcon, TrashIcon } from "lucide-react";
 
-export const CreateDatasetDialog = () => {
+export const CreateDatasetDialog = ({
+  onContinue,
+}: {
+  onContinue: () => void;
+}) => {
   const t = useTranslations("dataset-choice");
   const [open, setOpen] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const form = useForm<FormValues>({
     resolver: zodResolver(FormSchema),
@@ -41,16 +48,33 @@ export const CreateDatasetDialog = () => {
   const onSubmit = (data: FormValues) => {
     alert(JSON.stringify(data, null, 2));
     setOpen(false);
+    // chiamata per salvare i dati (se necessario)
+    onContinue();
   };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      setFile(files[0]);
+    }
+  };
+
+  // Funzione per gestire il click sul div
+  const handleFileInput = () => {
+    // Attiva il click sull'input file
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const isDisabled = !file || !form.formState.isValid;
 
   return (
     <>
-      {/* Button to open the dialog */}
       <Button onClick={() => setOpen(true)}>
         {t("create-custom-dataset-dialog.create")}
       </Button>
 
-      {/* Dialog Component */}
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent>
           <DialogHeader>
@@ -83,7 +107,6 @@ export const CreateDatasetDialog = () => {
                 )}
               />
 
-              {/* Description Field */}
               <FormField
                 control={form.control}
                 name="description"
@@ -104,10 +127,46 @@ export const CreateDatasetDialog = () => {
                   </FormItem>
                 )}
               />
+              {file ? (
+                <>
+                  <div className="flex items-center space-x-2 justify-between">
+                    <div className="flex items-center space-x-2 justify-between">
+                      <Button size="icon" disabled>
+                        <CheckIcon className="w-4 h-4" />
+                      </Button>
+                      <p>{file.name}</p>
+                    </div>
+                    <TrashIcon
+                      onClick={() => setFile(null)}
+                      className="w-4 h-4 cursor-pointer"
+                    />
+                  </div>
+                </>
+              ) : (
+                <div
+                  className="flex items-center space-x-2 cursor-pointer"
+                  onClick={handleFileInput}
+                >
+                  <Button variant="outline" size="icon">
+                    <PlusIcon className="w-4 h-4" />
+                  </Button>
+                  <Label htmlFor="json" className="cursor-pointer">
+                    {t("create-custom-dataset-dialog.upload-file")}
+                  </Label>
+                  <Input
+                    id="json"
+                    type="file"
+                    accept=".json"
+                    onChange={handleFileChange}
+                    ref={fileInputRef}
+                    className="hidden"
+                  />
+                </div>
+              )}
 
               {/* Submit Button */}
               <div className="flex justify-end">
-                <Button type="submit">
+                <Button type="submit" disabled={isDisabled}>
                   {t("create-custom-dataset-dialog.submit")}
                 </Button>
               </div>
--- src/features/dataset-choice/create-dataset-dialog/schema.ts
+++ src/features/dataset-choice/create-dataset-dialog/schema.ts
@@ -4,8 +4,8 @@ export const FormSchema = z.object({
   name: z.string().min(2, {
     message: "Name must be at least 2 characters.",
   }),
-  description: z.string().min(10, {
-    message: "Description must be at least 10 characters.",
+  description: z.string().min(5, {
+    message: "Description must be at least 5 characters.",
   }),
 });
 
