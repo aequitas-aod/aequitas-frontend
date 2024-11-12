24d90908ff8fd4e7717bea38cda8e7681cfb2b16
Jessica Corrias <corrias.jessica93@gmail.com>
2024-11-12 11:15:03 +0100

    feat: enhance RadioItem component styling and improve text semantics

--- src/components/molecules/RadioItem/index.tsx
+++ src/components/molecules/RadioItem/index.tsx
@@ -15,24 +15,23 @@ export const RadioItem = ({
     <div
       key={option.id.code}
       onClick={() => onSelect(option.id.code)}
-      className={`flex items-center space-x-2 py-6 px-4 border rounded-lg cursor-pointer hover:bg-indigo-100 hover:border-indigo-100 hover:font-medium	
+      className={`flex items-center space-x-2 py-6 px-4 rounded-lg cursor-pointer hover:bg-indigo-100 hover:border-indigo-100 hover:font-medium	
         ${
           selected
-            ? "bg-indigo-200 border-indigo-300 font-extrabold"
-            : "border-gray-300"
+            ? "bg-indigo-200 border-indigo-300 font-extrabold "
+            : "border-gray-300 font-light"
         }`}
     >
       <RadioGroupItem value={option.id.code} checked={selected} />
-      <Label
+      <p
         className={`text-sm hover:font-medium text-black ${
           selected
             ? "text-indigo-950 font-extrabold"
             : "text-gray-900 font-light"
         }`}
-        htmlFor={option.id.code}
       >
         {option.text}
-      </Label>
+      </p>
     </div>
   );
 };
