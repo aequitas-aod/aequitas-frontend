9a467ad3cbbc31ceb4187ae8ab4ad25778cf3e9e
Jessica Corrias <corrias.jessica93@gmail.com>
2024-11-15 11:39:58 +0100

    feat: remove unused mock files and add new dataset and questionnaire mocks

--- mocks/4/mock.ts
+++
@@ -1,5 +0,0 @@
-import { features } from "../3/mock";
-
-export const getSensitiveFeatures = () => {
-  return features.filter((feature) => feature.sensitive === true);
-};
---
+++ mocks/correlation_matrix/custom-1.ts
@@ -0,0 +1,3 @@
+const data =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200"><circle cx="100" cy="100" r="80" stroke="black" stroke-width="3" fill="lightblue" /></svg>';
+export default data;
