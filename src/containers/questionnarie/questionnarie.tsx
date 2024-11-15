e2929c3737aa0fec59bb1fbbcb40ecdf80f5012f
Jessica Corrias <corrias.jessica93@gmail.com>
2024-11-15 11:41:58 +0100

    feat: update QuestionnaireContainer to include questionId prop for improved navigation and component interaction

--- src/containers/questionnarie/questionnarie.tsx
+++ src/containers/questionnarie/questionnarie.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { DataMitigationPage } from "@/features/data-mitigation/page";
-import { DatasetChoicePage } from "@/features/dataset-choice/page";
-import { DatasetViewPage } from "@/features/dataset-view/page";
-import { DependenciesPage } from "@/features/dependencies/page";
 import { DetectionPage } from "@/features/detection/page";
-import { FeatureViewPage } from "@/features/feature-view/page";
-import { useRouter, useSearchParams } from "next/navigation";
+
 import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { DatasetChoicePage } from "../dataset-choice";
+import { FeaturesViewPage } from "../features-view";
+import { DependenciesPage } from "../dependencies";
+import { DatasetViewPage } from "../dataset-view";
 
 export default function QuestionnaireContainer() {
   const router = useRouter();
@@ -26,10 +28,11 @@ export default function QuestionnaireContainer() {
 
   return (
     <div className="flex flex-col h-full">
-      {question === "1" && <DatasetChoicePage onNext={onNext} />}
-      {question === "2" && <DatasetViewPage onNext={onNext} />}
-      {question === "3" && <FeatureViewPage onNext={onNext} />}
-      {question === "4" && <DependenciesPage onNext={onNext} />}
+      {question === "1" && <DatasetChoicePage onNext={onNext} questionId={1} />}
+      {question === "2" && <DatasetViewPage onNext={onNext} questionId={2} />}
+      {question === "3" && <FeaturesViewPage onNext={onNext} questionId={2} />}
+      {question === "4" && <DependenciesPage onNext={onNext} questionId={3} />}
+
       {question === "5" && <DetectionPage onNext={onNext} />}
       {question === "6" && <DataMitigationPage onNext={onNext} />}
     </div>
