fd8efb5fafeffbf132954759c8b71d308b5ba790
Jessica Corrias <corrias.jessica93@gmail.com>
2024-11-15 11:41:05 +0100

    feat: wrap QuestionnaireContainer with QueryClientProvider for data fetching

--- src/app/[locale]/(private)/questionnaire/page.tsx
+++ src/app/[locale]/(private)/questionnaire/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import QuestionnaireContainer from "@/containers/questionnarie/questionnarie";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function ProjectPage() {
-  return <QuestionnaireContainer />;
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <QuestionnaireContainer />
+    </QueryClientProvider>
+  );
 }
