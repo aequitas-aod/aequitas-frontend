24a49fdd495470fa95fe4716f7fc46d7a32cd3f2
Jessica Corrias <corrias.jessica93@gmail.com>
2024-11-21 18:02:29 +0100

    refactor: reorganize imports and update question handling in QuestionnaireContainer

--- src/containers/questionnarie/questionnarie.tsx
+++ src/containers/questionnarie/questionnarie.tsx
@@ -1,7 +1,4 @@
 "use client";
-import { DataMitigationPage } from "@/features/data-mitigation/page";
-import { DetectionPage } from "@/features/detection/page";
-
 import { useState, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
@@ -9,6 +6,8 @@ import { DatasetChoicePage } from "../dataset-choice";
 import { FeaturesViewPage } from "../features-view";
 import { DependenciesPage } from "../dependencies";
 import { DatasetViewPage } from "../dataset-view";
+import { DataMitigationPage } from "../data-mitigation";
+import { DetectionPage } from "../detection";
 
 export default function QuestionnaireContainer() {
   const router = useRouter();
@@ -30,14 +29,12 @@ export default function QuestionnaireContainer() {
     <div className="flex flex-col h-full">
       {question === "1" && <DatasetChoicePage onNext={onNext} questionId={1} />}
       {question === "2" && <DatasetViewPage onNext={onNext} questionId={2} />}
-      {question === "3" && <FeaturesViewPage onNext={onNext} questionId={2} />}
-      {question === "4" && <DependenciesPage onNext={onNext} questionId={3} />}
-
-      {question === "5" && <DetectionPage onNext={onNext} />}
-      {question === "6" && <DataMitigationPage onNext={onNext} />}
-      {/*question === "7" && (
-        <XProcessAlgorithmPage onNext={onNext} questionId={7} />
-      )*/}
+      {question === "3" && <FeaturesViewPage onNext={onNext} questionId={3} />}
+      {question === "4" && <DependenciesPage onNext={onNext} questionId={4} />}
+      {question === "5" && <DetectionPage onNext={onNext} questionId={5} />}
+      {question === "6" && (
+        <DataMitigationPage onNext={onNext} questionId={6} />
+      )}
     </div>
   );
 }
