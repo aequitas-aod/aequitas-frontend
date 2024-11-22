cacbdd8693c8997775c121ec20508781ad1e70dd
Jessica Corrias <corrias.jessica93@gmail.com>
2024-11-22 16:33:02 +0100

    feat: enhance DatasetPreview component with value formatting and key cleaning functions

--- src/features/dataset-choice/dataset-preview/index.tsx
+++ src/features/dataset-choice/dataset-preview/index.tsx
@@ -6,23 +6,43 @@ export const DatasetPreview = ({
   description,
   details,
 }: {
-  title?: string;
-  description?: string;
-  details?: {
+  title: string;
+  description: string;
+  details: {
     [key: string]: string | number; // Dettagli del dataset
   };
 }) => {
+  const isValidDate = (date: string) => {
+    const parsedDate = new Date(date);
+    return !isNaN(parsedDate.getTime());
+  };
+
+  const formatValue = (value: string | number): string => {
+    if (typeof value === "number") {
+      return value.toString();
+    }
+    if (typeof value === "string" && isValidDate(value)) {
+      // Formatta la data nel formato "18 Oct 2024"
+      return new Intl.DateTimeFormat("en-GB", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+      }).format(new Date(value));
+    }
+    return value.toString(); // Assicura sempre il ritorno come stringa
+  };
+
+  const formatKey = (key: string) => {
+    const cleanedKey = key.replace(/[-_]/g, " "); // Rimuove tutti i "-" e "_"
+    return cleanedKey.charAt(0).toUpperCase() + cleanedKey.slice(1);
+  };
+
   return (
     <div className="flex flex-col border p-4 shadow-md rounded-md bg-white h-full">
-      {title ? (
-        <p className="text-2xl text-primary-950">{title}</p>
-      ) : (
-        <p className="text-base text-neutral-300">No dataset selected</p>
-      )}
+      <p className="text-2xl text-primary-950">{title}</p>
       {description && (
         <p className="text-neutral-400 text-sm mt-2">{description}</p>
       )}
-
       <div className="flex flex-col mt-auto">
         {details && (
           <>
@@ -31,10 +51,10 @@ export const DatasetPreview = ({
                 key={key}
                 className="flex justify-start items-center gap-8 mt-4"
               >
-                <Label className="text-neutral-400 text-sm min-w-12">
-                  {key.charAt(0).toUpperCase() + key.slice(1)}
+                <Label className="text-neutral-400 text-sm min-w-16">
+                  {formatKey(key)}
                 </Label>
-                <Input readOnly value={value} className="w-48" />
+                <Input readOnly value={formatValue(value)} className="w-auto" />
               </div>
             ))}
           </>
