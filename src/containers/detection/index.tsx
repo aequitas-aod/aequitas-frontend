c8c7ac8855f47d2012b8cb1b2729f62d8a89003e
Jessica Corrias <corrias.jessica93@gmail.com>
2024-11-22 17:51:37 +0100

    feat: integrate useFeaturesContext to fetch and filter sensitive features for DetectionPage

--- src/containers/detection/index.tsx
+++ src/containers/detection/index.tsx
@@ -1,4 +1,5 @@
-import { useQuestionnaire } from "@/api/hooks";
+import { useFeaturesContext, useQuestionnaire } from "@/api/hooks";
+import { FeaturesResponse } from "@/api/types";
 import { Detection } from "@/features/detection/page";
 import React from "react";
 
@@ -7,23 +8,65 @@ interface QuestionnairePageProps {
   onNext: () => void;
 }
 
+export type FeatureName = {
+  text: string;
+  value: FeaturesResponse;
+};
+
 export const DetectionPage: React.FC<QuestionnairePageProps> = ({
   questionId,
   onNext,
 }) => {
-  const { data, isLoading, error } = useQuestionnaire(questionId);
+  const {
+    data: questionnaireData,
+    isLoading: isLoadingQuestionnaireData,
 
-  if (isLoading) {
+    error: errorQuestionnaireData,
+  } = useQuestionnaire(questionId);
+  const {
+    data: featuresData,
+    isLoading: featuresLoading,
+    error: featuresError,
+  } = useFeaturesContext("custom-1");
+
+  if (isLoadingQuestionnaireData || featuresLoading) {
     return <div>Loading...</div>;
   }
 
-  if (error instanceof Error) {
-    return <div>Error: {error.message}</div>;
+  if (errorQuestionnaireData || featuresError) {
+    return <div>Error...</div>;
   }
 
-  if (!data) {
-    return <div>No data available</div>;
+  if (!questionnaireData || !featuresData) {
+    return <div>No data...</div>;
   }
 
-  return <Detection onNext={onNext} />;
+  // Filtrare solo le caratteristiche sensibili
+  const sensitiveFeatures = Object.keys(featuresData)
+    .filter((key) => featuresData[key].sensitive) // Filtra le chiavi con sensitive = true
+    .reduce((obj, key) => {
+      obj[key] = featuresData[key]; // Aggiungi ogni chiave e valore a un nuovo oggetto
+      return obj;
+    }, {} as FeaturesResponse); // Usa il tipo FeaturesResponse per il risultato
+
+  console.log(sensitiveFeatures);
+
+  console.log(sensitiveFeatures);
+
+  console.log(sensitiveFeatures);
+
+  // per ogni text dentro questionnarieData, gli aggiungo come valore il valore di featuresData
+  const featureName: FeatureName[] = questionnaireData.answers
+    .map((question) => question.text)
+    .map((text) => {
+      return {
+        text,
+
+        value: sensitiveFeatures,
+      };
+    });
+
+  console.log(featureName);
+
+  return <Detection onNext={onNext} data={featureName} />;
 };
