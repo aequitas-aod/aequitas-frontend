b25f89544a2d1eff13695d65ee8ecd2f483b6dc3
Jessica Corrias <corrias.jessica93@gmail.com>
2024-12-03 23:22:09 +0100

    feat: integrate Sidebar component into ProjectPage; refactor Sidebar to use centralized store for state management

--- src/app/[locale]/(private)/questionnaire/page.tsx
+++ src/app/[locale]/(private)/questionnaire/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 import QuestionnaireContainer from "@/containers/questionnarie";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { sidebarItems } from "../../../../../mocks/sidebar";
+import { Sidebar } from "@/components/organisms/Sidebar/Sidebar";
 
 export default function ProjectPage() {
+  const menuItems = sidebarItems;
   return (
     <QueryClientProvider client={new QueryClient()}>
-      <QuestionnaireContainer />
+      <Sidebar menuItems={menuItems} />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <QuestionnaireContainer />
+      </div>
     </QueryClientProvider>
   );
 }
--- src/components/organisms/Sidebar/Sidebar.tsx
+++ src/components/organisms/Sidebar/Sidebar.tsx
@@ -10,9 +10,8 @@ import {
 } from "@/components/ui/dialog";
 import { IMenuItem } from "@/components/molecules/MenuItem";
 import { Menu, IMenuItemWithState } from "@/components/molecules/Menu";
-import { useSearchParams } from "next/navigation";
 import { IoInformationCircleOutline } from "react-icons/io5";
-import { useSidebarStore } from "@/store/sidebarStore";
+import { useStore } from "@/store/store";
 import { useDeleteQuestionnaireMutation } from "@/api/hooks";
 
 type SidebarProps = {
@@ -20,10 +19,10 @@ type SidebarProps = {
 };
 
 export const Sidebar = ({ menuItems }: SidebarProps) => {
-  const { currentStep, setCurrentStep } = useSidebarStore();
+  const { currentStep, setCurrentStep } = useStore();
   const { mutate } = useDeleteQuestionnaireMutation({
     onSuccess: () => {
-      console.log("Step deleted successfully  ");
+      console.log("Step deleted successfully");
     },
   });
 
@@ -31,8 +30,7 @@ export const Sidebar = ({ menuItems }: SidebarProps) => {
   const [dialogTitle, setDialogTitle] = useState<string>("");
   const [dialogContent, setDialogContent] = useState<string>("");
 
-  const { menuItems: dynamicMenuItems, setInitialMenuItems } =
-    useSidebarStore();
+  const { menuItems: dynamicMenuItems, setInitialMenuItems } = useStore();
 
   useEffect(() => {
     setInitialMenuItems(menuItems);
@@ -46,7 +44,6 @@ export const Sidebar = ({ menuItems }: SidebarProps) => {
 
   const currentIndex = useMemo(
     () => dynamicMenuItems.findIndex((item) => item.step === currentStep) + 1,
-
     [currentStep, dynamicMenuItems]
   );
 
@@ -64,22 +61,17 @@ export const Sidebar = ({ menuItems }: SidebarProps) => {
   );
 
   const handlePastClick = (path: number) => {
-    // Step 1: Calcola i passi da eliminare
     const stepsToDelete = [];
     for (let step = currentIndex; step > path; step--) {
       stepsToDelete.push(step);
     }
 
-    // Step 2: Esegui le richieste DELETE per ogni step
     try {
       for (const step of stepsToDelete) {
         mutate(step);
       }
-
-      // Step 3: Una volta eliminati i passi, aggiorna lo stato e torna al passo precedente
       setCurrentStep(path);
     } catch (error) {
-      // Gestisci gli errori qui, magari notificando l'utente
       console.error("An error occurred while deleting steps: ", error);
     }
   };
