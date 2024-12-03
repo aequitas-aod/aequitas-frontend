81ade82ce3bfc06799242654d3b452f581feac24
Jessica Corrias <corrias.jessica93@gmail.com>
2024-12-03 12:35:31 +0100

    feat: add DatasetView component for displaying parsed CSV data

---
+++ src/features/dm-results/sections/dataset-view.tsx
@@ -0,0 +1,58 @@
+import { useTranslations } from "next-intl";
+import Papa from "papaparse";
+import { useEffect, useState } from "react";
+import { processDataset } from "@/lib/utils";
+import { CsvData, ParsedDataset } from "@/types/types";
+import { toast } from "@/hooks/use-toast";
+import { DatasetViewTable } from "@/features/dataset-view/table";
+import { useDatasetContext } from "@/api/hooks";
+
+export const DatasetView = () => {
+  const t = useTranslations("dataset-view");
+  const [data, setData] = useState<ParsedDataset[]>([]); // Stato per i dati del CSV
+  const [columns, setColumns] = useState<string[]>([]); // Stato per le colonne dinamiche
+
+  const { data: contextData, isLoading, error } = useDatasetContext("custom-1");
+
+  useEffect(() => {
+    const parseCsv = (csv: string) => {
+      const result = Papa.parse<CsvData>(csv, {
+        header: true,
+        skipEmptyLines: true,
+      });
+
+      if (result.errors.length > 0) {
+        toast({
+          title: t("errors.parsing-csv"),
+          description: result.errors[0].message,
+          variant: "destructive",
+        });
+      } else {
+        // Processa i dati e converte i valori booleani, stringhe, array e oggetti
+        const processedData = processDataset(result.data);
+        setData(processedData);
+        if (result.data.length > 0) {
+          setColumns(Object.keys(result.data[0]));
+        }
+      }
+    };
+    if (!contextData) {
+      return;
+    }
+    parseCsv(contextData);
+  }, [contextData, t]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error instanceof Error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!contextData) {
+    return <div>No data available</div>;
+  }
+
+  return <DatasetViewTable data={data} columns={columns} />;
+};
