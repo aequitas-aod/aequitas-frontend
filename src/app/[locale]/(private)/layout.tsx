19b5eaa65cb16d3327053184e22846b2e21e57ed
Jessica Corrias <corrias.jessica93@gmail.com>
2024-12-03 23:21:51 +0100

    feat: remove Sidebar component and associated menuItems logic from PrivateLayout

--- src/app/[locale]/(private)/layout.tsx
+++ src/app/[locale]/(private)/layout.tsx
@@ -1,6 +1,4 @@
 import { Footer } from "@/components/organisms/Footer/Footer";
-import { Sidebar } from "@/components/organisms/Sidebar/Sidebar";
-import { sidebarItems } from "../../../../mocks/sidebar";
 import { routing } from "@/i18n/routing";
 import { setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
@@ -12,13 +10,6 @@ export default async function PrivateLayout({
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 }>) {
-  const menuItems = await Promise.all(
-    sidebarItems.map(async (item) => ({
-      ...item,
-      href: `/${(await params).locale}/${item.path}`,
-    }))
-  );
-
   const locale = (await params).locale;
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as any)) {
@@ -31,14 +22,8 @@ export default async function PrivateLayout({
   return (
     <div className="flex flex-col h-[100vh]">
       <div className="flex flex-1 overflow-hidden p-4 gap-4">
-        {/* Sidebar occupa tutta l'altezza disponibile */}
-        <Sidebar menuItems={menuItems} />
-
-        {/* Contenuto principale */}
-        <div className="flex-1 flex flex-col overflow-hidden">
-          {/* Contenuto scrollabile */}
-          {children}
-        </div>
+        {/* Contenuto scrollabile */}
+        {children}
       </div>
       {/* Footer fisso in basso */}
       <Footer />
