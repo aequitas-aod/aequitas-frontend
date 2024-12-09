52b1a229ebb5c556a4b454a4222217d838f48fa1
Jessica Corrias <corrias.jessica93@gmail.com>
2024-12-09 13:19:05 +0100

    feat: add resetMenuItems method to SidebarState for improved state management

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,6 +7,7 @@ interface SidebarState {
   setCurrentStep: (step: number) => void;
   menuItems: IMenuItem[];
   setInitialMenuItems: (items: IMenuItem[]) => void;
+  resetMenuItems: () => void;
   addMenuItem: (item: IMenuItem) => void;
   deleteMenuItem: (item: IMenuItem) => void;
   datasetKey: string | null; // Ora può essere anche null
@@ -17,15 +18,13 @@ interface SidebarState {
 export const useStore = create<SidebarState>((set, get) => ({
   currentStep: 1,
   setCurrentStep: (step) => set({ currentStep: step }),
-
   menuItems: sidebarItems,
   setInitialMenuItems: (items) => set({ menuItems: items }),
-
+  resetMenuItems: () => set({ menuItems: sidebarItems }),
   addMenuItem: (item) =>
     set((state) => ({
       menuItems: [...state.menuItems, item],
     })),
-
   deleteMenuItem: (item) =>
     set((state) => ({
       menuItems: state.menuItems.filter((i) => i.id !== item.id),
@@ -33,7 +32,6 @@ export const useStore = create<SidebarState>((set, get) => ({
 
   datasetKey: "custom-1",
   setDatasetKey: (name) => set({ datasetKey: name }),
-
   incrementDatasetKey: () => {
     set((state) => {
       const datasetPrefix = "custom-";
@@ -49,7 +47,6 @@ export const useStore = create<SidebarState>((set, get) => ({
       // Controlla se il parsing ha avuto successo, altrimenti inizia da 1
       const newNumber = isNaN(currentNumber) ? 1 : currentNumber + 1;
       const newDatasetKey = `${datasetPrefix}${newNumber}`;
-
       return { datasetKey: newDatasetKey };
     });
   },
