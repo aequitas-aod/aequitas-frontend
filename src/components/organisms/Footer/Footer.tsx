49ddac54e15c2c5a831096253d7cb7dea193f98b
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2024-12-12 13:19:27 +0100

    feat: show backend url in foot for the sake of debugging

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,9 +1,18 @@
 import { AequitasLogoImage } from "@/components/contents/images";
 
+const ApiUrlInfo = () => {
+  return (
+    <span>
+      Backend URL: {process.env.NEXT_PUBLIC_BACKEND_URL || "not set"}
+    </span>
+  );
+};
+
 export const Footer = () => {
   return (
     <footer className="w-full bg-gray-800 text-white text-center py-2 px-12">
       <AequitasLogoImage maxW="sm" hFull={false} />
+      <ApiUrlInfo />
     </footer>
   );
 };
