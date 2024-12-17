c7cf3e0cfad48e038406e0ee8125a6f5680d9807
Jessica Corrias <corrias.jessica93@gmail.com>
2024-12-17 13:20:02 +0100

    feat: add useQuestionnaireData hook for managing questionnaire state and navigation

---
+++ src/hooks/useQuestionnaireData.ts
@@ -0,0 +1,43 @@
+/*
+ faccio la questionnaire: ti dice tutto il questionario
+ da questa popolo la menuItems
+ prendo anche l'ultimo step e faccio la get context relativa alla 
+*/
+
+import {
+  useQuestionnaireById,
+  useQuestionnaireList,
+} from "@/api/questionnaire";
+import { useStore } from "@/store/store";
+
+export const useQuestionnaireData = () => {
+  const { currentStep, setCurrentStep, menuItems, setDatasetKey } = useStore();
+
+  // 1: AGGIORNO LA SIDEBAR
+  const {
+    data: questions,
+    isLoading: questionsLoading,
+    error: questionsError,
+  } = useQuestionnaireList();
+
+  const questionNumber = currentStep; // questions.length
+
+  // setMenuItems(questions)
+
+  // TODO: 2: PRENDO IL DATASET CORRENTE DALLA CONTEXT
+
+  // per prendere
+
+  const onNext = () => {
+    setCurrentStep(currentStep + 1);
+    // dopo andiamo a rifare la chiamata della useQuestionnaireList
+  };
+
+  const currentQuestion = menuItems.find((step) => step.step === currentStep);
+
+  return {
+    onNext,
+    menuItems,
+    currentQuestion,
+  };
+};
