229ca2731f6177f7b90ba3a632421d903a24255f
Jessica Corrias <corrias.jessica93@gmail.com>
2024-12-18 16:59:26 +0100

    feat: add feedback button and update dataset selection logic for improved user interaction

--- src/features/dataset-selection/index.tsx
+++ src/features/dataset-selection/index.tsx
@@ -6,27 +6,39 @@ import { RadioGroup } from "@/components/ui/radio-group";
 import { RadioItem } from "@/components/molecules/RadioItem";
 import { QuestionnaireLayout } from "@/components/molecules/Layout/layout";
 
-import type { AnswerResponse, QuestionnaireResponse } from "@/api/types";
 import { DatasetPreview } from "./dataset-preview";
-import { useStore } from "@/store/store";
+import { useAequitasStore } from "@/store/store";
 import { QuestionnaireBanner } from "@/components/molecules/Layout/banner";
+import {
+  EnhancedAnswerResponse,
+  Questionnaire,
+} from "@/containers/dataset-selection";
+import { useUpdateQuestionnaire } from "@/api/questionnaire";
 
 export const DatasetSelection = ({
   data,
   onNext,
 }: {
-  data: QuestionnaireResponse;
+  data: Questionnaire;
   onNext: () => void;
 }) => {
   const t = useTranslations("DatasetSelection");
-  const [selected, setSelected] = useState<AnswerResponse | null>(null);
-  const { setDatasetKey } = useStore();
+  const [selected, setSelected] = useState<EnhancedAnswerResponse | null>(null);
+  const { setDatasetKey, currentStep } = useAequitasStore();
 
+  const { mutate, isPending } = useUpdateQuestionnaire({
+    onSuccess: () => {
+      // TODO: remove when the API is ready
+      setDatasetKey("custom-1");
+      //
+      onNext();
+    },
+  });
   const options = data.answers;
 
   const onSelect = (value: string) => {
     const selectedOption =
-      options.find((option) => option.id.code === value) || null;
+      options?.find((option) => option.id.code === value) || null;
     if (!selectedOption) {
       return;
     }
@@ -37,9 +49,10 @@ export const DatasetSelection = ({
     if (!selected) {
       return;
     }
-    setDatasetKey("custom-1");
-    // chiamata per salvare i dati (se necessario)
-    onNext();
+    mutate({
+      n: currentStep,
+      answer_ids: [selected.id],
+    });
   };
 
   return (
@@ -57,16 +70,18 @@ export const DatasetSelection = ({
         classNameWrapper="!overflow-hidden"
         className="!bg-neutral-50"
       >
-        <QuestionnaireBanner>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        <QuestionnaireBanner
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
-          {/*<p>{t("create-custom-dataset")}</p>
-          <CreateDatasetDialog onContinue={onNext} />*/}
+          culpa qui officia deserunt mollit anim id est laborum"
+        >
+          <Button onClick={() => alert("feedback")} variant="secondary">
+            {t("buttons.feedback")}
+          </Button>
         </QuestionnaireBanner>
         <div className="flex justify-between space-x-4 p-8 bg-neutral-50 rounded-b-md flex-1">
           <div id="dataset-selection" className="flex-1">
@@ -75,11 +90,11 @@ export const DatasetSelection = ({
             </p>
             <p className="text-[#64748B] text-sm">{t("subtitle")}</p>
             <RadioGroup className="mt-4">
-              {options.map((option) => (
+              {options?.map((option) => (
                 <RadioItem
                   key={option.id.code}
                   option={option}
-                  selected={selected?.id.code === option.id.code}
+                  selected={selected?.text === option.text}
                   onSelect={onSelect}
                 />
               ))}
@@ -88,9 +103,12 @@ export const DatasetSelection = ({
           {selected && (
             <div className="flex-1 h-full">
               <DatasetPreview
+                questionNumber={questionNumber}
+                selected={selected}
                 title={selected.text}
                 description={selected.description || ""}
                 details={selected.details}
+                onNext={onNext}
               />
             </div>
           )}
