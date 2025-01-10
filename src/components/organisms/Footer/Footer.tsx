e708c9d7c64f49874d633138287089d9b4d69543
Jessica Corrias <jessica.corrias@devpunks.com>
2025-01-10 18:51:08 +0100

    feat: add reset questionnaire button in Footer component

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,4 +1,9 @@
+"use client";
 import { AequitasLogoImage } from "@/components/contents/images";
+import { Button } from "@/components/ui/button";
+import { BACKEND_URL, PROJECT_CODE } from "@/config/constants";
+import axios from "axios";
+import { redirect } from "next/navigation";
 
 const ApiUrlInfo = () => {
   return (
@@ -7,10 +12,17 @@ const ApiUrlInfo = () => {
 };
 
 export const Footer = () => {
+  const onResetQuestionnaire = async () => {
+    const res = await axios.delete(
+      `${BACKEND_URL}/projects/${PROJECT_CODE}/questionnaire`
+    );
+    redirect("/en");
+  };
   return (
     <footer className="w-full bg-gray-800 text-white text-center py-2 px-12">
       <AequitasLogoImage maxW="sm" hFull={false} />
       <ApiUrlInfo />
+      <Button onClick={onResetQuestionnaire}>Reset all</Button>
     </footer>
   );
 };
