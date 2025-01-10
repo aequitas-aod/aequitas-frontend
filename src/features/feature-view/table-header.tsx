76f02a2a99815e47194da90465dd9b065bf4d085
Jessica Corrias <jessica.corrias@devpunks.com>
2025-01-10 16:27:17 +0100

    feat: add FeatureViewTableHeader component for dynamic table headers

---
+++ src/features/feature-view/table-header.tsx
@@ -0,0 +1,35 @@
+import { TableHeader, TableRow, TableHead } from "@/components/ui/table";
+
+import { TARGET, SENSITIVE, DISTRIBUTION } from "@/config/constants";
+
+export const FeatureViewTableHeader = ({
+  columns,
+  showSelectRow = false,
+}: {
+  columns: string[];
+  showSelectRow?: boolean;
+}) => {
+  return (
+    <TableHeader className="sticky top-0 z-10 bg-neutral-50">
+      <TableRow>
+        {showSelectRow && (
+          <TableHead className="text-center bg-neutral-50 text-neutral-400 border-b-2 border-neutral-200 px-6 border-r-2"></TableHead>
+        )}
+        {columns.map((key, colIndex) => (
+          <TableHead
+            key={key}
+            id={key}
+            className={`text-center bg-neutral-100 text-neutral-400 border-b-2 border-neutral-200 px-6 ${
+              key === TARGET && "!bg-primary-950 !text-white !px-0 !w-16"
+            } ${key === SENSITIVE && "!bg-primary-900 !text-white !w-16 !px-0"} 
+                    ${key === DISTRIBUTION && "!w-[600px]"}
+                  ${colIndex !== columns.length - 1 && "border-r-2"}
+                  `}
+          >
+            {key}
+          </TableHead>
+        ))}
+      </TableRow>
+    </TableHeader>
+  );
+};
