bfcc05343638c87d041008b1cd6c644f37c45cf1
Jessica Corrias <jessica.corrias@devpunks.com>
2025-01-22 00:49:26 +0100

    feat: add delete functionality to useQuestionnaireData hook with error handling

--- src/hooks/useQuestionnaireData.ts
+++ src/hooks/useQuestionnaireData.ts
@@ -1,15 +1,26 @@
-import { useQuestionnaireList } from "@/api/questionnaire";
+import {
+  useDeleteQuestionnaireById,
+  useQuestionnaireList,
+} from "@/api/questionnaire";
 import { useQueryClient } from "@tanstack/react-query";
 
 export const useQuestionnaireData = () => {
   const queryClient = useQueryClient();
   const { data: questions, isLoading, error } = useQuestionnaireList();
 
+  const { mutate } = useDeleteQuestionnaireById({
+    onSuccess: () => {
+      console.log("Step deleted successfully");
+    },
+  });
+
   const sidebarItems =
     questions?.map((question, index) => ({
       id: question.id.code,
       step: index + 1,
-      name: question.id.code,
+      name: question.id.code
+        .replace(/([A-Z])/g, " $1")
+        .replace(/^./, (str) => str.toUpperCase()),
     })) || [];
 
   const invalidateQuestionnaire = () => {
@@ -21,7 +32,27 @@ export const useQuestionnaireData = () => {
     invalidateQuestionnaire();
   };
 
-  const onDelete = () => {
+  const currentIndex = questions?.length || 0;
+
+  const onDelete = (path: number) => {
+    if (path === currentIndex) {
+      return;
+    }
+    // TODO: Check if this is the correct behavior
+    if (!questions || !questions.length) {
+      return;
+    }
+    const stepsToDelete = [];
+    for (let step = currentIndex; step > path; step--) {
+      stepsToDelete.push(step);
+    }
+    try {
+      for (const step of stepsToDelete) {
+        mutate({ n: step });
+      }
+    } catch (error) {
+      console.error("An error occurred while deleting steps: ", error);
+    }
     // Placeholder for additional logic
     invalidateQuestionnaire();
   };
