daeeab676b6bf390a8780dbf0ce6d517d9e7768d
Jessica Corrias <jessica.corrias@devpunks.com>
2025-01-22 00:22:29 +0100

    refactor: remove Sidebar mock and update Menu components to streamline navigation handling

--- src/components/molecules/Menu/index.tsx
+++ src/components/molecules/Menu/index.tsx
@@ -1,22 +1,22 @@
 import { MenuItem, type IMenuItem } from "../MenuItem";
 
 export interface IMenuItemWithState extends IMenuItem {
-  state: "past" | "current" | "future";
+  state: "past" | "current";
 }
 
 interface MenuProps {
   items: IMenuItemWithState[];
-  onNavigate: (path: number) => void;
+  onNavigateBack: (path: number) => void;
 }
 
-export const Menu = ({ items, onNavigate }: MenuProps) => {
+export const Menu = ({ items, onNavigateBack }: MenuProps) => {
   return (
     <nav className="flex flex-col space-y-4">
       {items.map((item) => (
         <MenuItem
           key={`${item.id}-${item.step}`}
           item={item}
-          onNavigate={onNavigate}
+          onNavigateBack={onNavigateBack}
         />
       ))}
     </nav>
--- src/components/organisms/Sidebar/Sidebar.tsx
+++ src/components/organisms/Sidebar/Sidebar.tsx
@@ -2,54 +2,23 @@
 
 import { IMenuItem } from "@/components/molecules/MenuItem";
 import { Menu, IMenuItemWithState } from "@/components/molecules/Menu";
-import { IoInformationCircleOutline } from "react-icons/io5";
-import { useDeleteQuestionnaireById } from "@/api/questionnaire";
-import { useQuestionnaireData } from "@/hooks/useQuestionnaireData";
 
 type SidebarProps = {
   menuItems: IMenuItem[];
+  onDelete: (n: number) => void;
 };
 
-export const Sidebar = ({ menuItems }: SidebarProps) => {
-  const { onDelete } = useQuestionnaireData();
-
-  const { mutate } = useDeleteQuestionnaireById({
-    onSuccess: () => {
-      console.log("Step deleted successfully");
-    },
-  });
-
+export const Sidebar = ({ menuItems, onDelete }: SidebarProps) => {
   const currentIndex = menuItems[menuItems.length - 1].step;
 
   const parsedMenuItems: IMenuItemWithState[] = menuItems.map((item) => ({
     ...item,
-    icon: <IoInformationCircleOutline />,
-    state:
-      currentIndex < item.step
-        ? "future"
-        : currentIndex === item.step
-          ? "current"
-          : "past",
+    state: currentIndex === item.step ? "current" : "past",
   }));
-
-  const handlePastClick = (path: number) => {
-    const stepsToDelete = [];
-    for (let step = currentIndex; step > path; step--) {
-      stepsToDelete.push(step);
-    }
-    try {
-      for (const step of stepsToDelete) {
-        mutate({ n: step });
-      }
-      onDelete();
-    } catch (error) {
-      console.error("An error occurred while deleting steps: ", error);
-    }
-  };
 
   return (
     <div className="h-full flex flex-col overflow-auto">
-      <Menu items={parsedMenuItems} onNavigate={handlePastClick} />
+      <Menu items={parsedMenuItems} onNavigateBack={onDelete} />
     </div>
   );
 };
